563fa534197f61e4ca23ba3612320b4d2ee1811d
Maksym Kolesnyk <brhumaan@gmail.com>
2019-04-03 12:22:49 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// eslint --install
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    // "quotes": [2, 'single'],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "SwaggerEditor": false,
+        "$": false,
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+
+    "rules": {
+        "linebreak-style": 0,
+        "indent": [2, 4],
+        "no-console": 0
+    }
+};
